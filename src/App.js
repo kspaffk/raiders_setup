4bc7d58c6df4d014fe4f8dfbbd315d59ee5fd098
Kent Spafford <spafcoding@gmail.com>
2021-01-19 20:16:34 -0700

    update readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let locations = [
+    {
+      name: 'fortress1',
+      plunder: 2,
+      plunder_arr: []
+    },
+    {
+      name: 'fortress2',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'fortress3',
+      plunder: 2,
+      plunder_arr: []
+    },
+    {
+      name: 'fortress4',
+      plunder: 2,
+      plunder_arr: []
+    },
+    {
+      name: 'fortress5',
+      plunder: 2,
+      plunder_arr: []
+    },
+    {
+      name: 'fortress6',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'monastery1',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'monastery2',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'monastery3',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'monastery4',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'outpost1',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'outpost2',
+      plunder: 3,
+      plunder_arr: []
+    },    {
+      name: 'outpost3',
+      plunder: 4,
+      plunder_arr: []
+    },    {
+      name: 'outpost4',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'harbor1',
+      plunder: 3,
+      plunder_arr: []
+    },
+    {
+      name: 'harbor2',
+      plunder: 4,
+      plunder_arr: []
+    },    {
+      name: 'harbor3',
+      plunder: 4,
+      plunder_arr: []
+    },    {
+      name: 'harbor4',
+      plunder: 4,
+      plunder_arr: []
+    },    {
+      name: 'harbor5',
+      plunder: 3,
+      plunder_arr: []
+    },    {
+      name: 'harbor6',
+      plunder: 4,
+      plunder_arr: []
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        locations.map(location => <div>{JSON.stringify(location)}</div>)
+      }
+      <button>
+        Setup!
+      </button>
     </div>
   );
 }
